3163e2a6fc207e3494e69a8df84a96ae7ca139a3
nikolasmushroom <dashagribok0905@gmail.com>
2024-11-13 23:38:37 +0300

    Was refactored to a custom-component Header.tsx

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -1,33 +1,23 @@
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-import {MenuButton} from "./MenuButton";
-import Switch from "@mui/material/Switch";
-import AppBar from "@mui/material/AppBar";
+import styles from './Header.module.css'
+import menuIconImage from './../../../asserts/burger.svg'
 import React from "react";
-import {changeThemeModeAC} from "./app/app-reducer";
-import {getTheme} from "./common/theme/theme";
-import {useAppDispatch, useAppSelector} from "./app/hook";
+import { Button } from "common/components/Button/Button";
+
 export const Header = () => {
-    const themeMode = useAppSelector(state => state.app.themeMode)
-    const dispatch = useAppDispatch()
-    const theme = getTheme(themeMode)
-    const changeModeHandler = () => {
-        dispatch(changeThemeModeAC(themeMode === "light" ? "dark" : 'light'))
-    }
-    return (
-        <AppBar position="static" sx={{mb: '30px'}}>
-            <Toolbar sx={{display: 'flex', justifyContent: 'space-between'}}>
-                <IconButton color="inherit">
-                    <MenuIcon/>
-                </IconButton>
-                <div>
-                    <MenuButton>Login</MenuButton>
-                    <MenuButton>Logout</MenuButton>
-                    <MenuButton background={theme.palette.primary.dark}>Faq</MenuButton>
-                    <Switch color={'default'} onChange={changeModeHandler}/>
-                </div>
-            </Toolbar>
-        </AppBar>
-    )
+
+  return (
+    <div className={styles.header}>
+      <div className={styles.toolBar}>
+        <div className={styles.burgerMenu}>
+          <img src={menuIconImage} alt="burger icon" className={styles.burgerMenuImage}/>
+        </div>
+        <div className={styles.buttonContainer}>
+          <Button>Login</Button>
+          <Button>Logout</Button>
+          <Button>Faq</Button>
+          <Button>Switch theme</Button>
+        </div>
+      </div>
+    </div>
+  )
 }
