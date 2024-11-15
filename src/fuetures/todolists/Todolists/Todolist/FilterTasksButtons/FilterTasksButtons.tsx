3d91c29f024493ffb2ed976ebbb539939bb6acf3
nikolasmushroom <dashagribok0905@gmail.com>
2024-11-15 23:43:01 +0300

    Was refactored component and were added new styles.

--- src/fuetures/todolists/Todolists/Todolist/FilterTasksButtons/FilterTasksButtons.tsx
+++ src/fuetures/todolists/Todolists/Todolist/FilterTasksButtons/FilterTasksButtons.tsx
@@ -1,37 +1,33 @@
-import {filterButtonsContainerSx} from "./Todolist.styles";
-import Button from "@mui/material/Button";
-import Box from "@mui/material/Box";
-import {FilterValuesType, TodolistType} from "./app/App";
-import {changeTodolistFilter} from "./model/todolists-reducer";
-import {useAppDispatch} from "./app/hook";
+
+import { changeTodolistFilter, FilterValuesType, TodolistDomainType } from "../../../../../model/todolists-reducer";
+import { useAppDispatch } from "common/hooks";
+import { Button } from "common/components/Button/Button";
+import styles from './FilterTasksButtons.module.css'
 type FilterTasksButtonsPropsType = {
-    todolist : TodolistType
-}
-export const FilterTasksButtons = ({todolist} : FilterTasksButtonsPropsType) => {
-    const dispatch = useAppDispatch()
-    const changeFilter = (filter: FilterValuesType, id: string) => {
-        dispatch(changeTodolistFilter({id, filter}))
-    }
-    return (
-        <Box sx={filterButtonsContainerSx}>
-            <Button
-                variant={todolist.filter === 'all' ? 'outlined' : 'text'}
-                color={'inherit'}
-                onClick={() => changeFilter('all', todolist.id)}>
-                All
-            </Button>
-            <Button
-                variant={todolist.filter === 'active' ? 'outlined' : 'text'}
-                color={'primary'}
-                onClick={() => changeFilter('active', todolist.id)}>
-                Active
-            </Button>
-            <Button
-                variant={todolist.filter === 'completed' ? 'outlined' : 'text'}
-                color={'secondary'}
-                onClick={() => changeFilter('completed', todolist.id)}>
-                Completed
-            </Button>
-        </Box>
-    )
-}
+  todolist: TodolistDomainType;
+};
+export const FilterTasksButtons = ({ todolist }: FilterTasksButtonsPropsType) => {
+  const dispatch = useAppDispatch();
+  const changeFilter = (filter: FilterValuesType, id: string) => {
+    dispatch(changeTodolistFilter({ id, filter }));
+  };
+  return (
+    <div>
+      <Button className={`${styles.button} ${todolist.filter === 'all' ? styles.active : ''}`} onClick={() => changeFilter("all", todolist.id)}>
+        All
+      </Button>
+      <Button
+        className={`${styles.button} ${todolist.filter === 'active' ? styles.active : ''}`}
+        onClick={() => changeFilter("active", todolist.id)}
+      >
+        Active
+      </Button>
+      <Button
+        className={`${styles.button} ${todolist.filter === 'completed' ? styles.active : ''}`}
+        onClick={() => changeFilter("completed", todolist.id)}
+      >
+        Completed
+      </Button>
+    </div>
+  );
+};
