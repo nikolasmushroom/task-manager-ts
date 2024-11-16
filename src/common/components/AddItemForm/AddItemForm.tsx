f7990d4a18fcfbd1722d21a30a4cf1fa13cf98a6
nikolasmushroom <dashagribok0905@gmail.com>
2024-11-16 23:12:00 +0300

    AddItemForm.tsx was refactored to universal component.

--- src/common/components/AddItemForm/AddItemForm.tsx
+++ src/common/components/AddItemForm/AddItemForm.tsx
@@ -1,26 +1,28 @@
 import {useAddItemForm} from "../../hooks";
 import React from "react";
-import { Button } from "common/components/Button/Button";
+import addIcon from './../../../asserts/addItem.png'
+import { TextInput } from "common/components/Input/TextInput";
+import styles from "./AddItemForm.module.css";
+import { IconButton } from "common/components/Button/IconButton";
 
 type PropsType = {
   addItem: (title: string) => void;
   disabled? :boolean
+  buttonText? : string
 };
 
-export const AddItemForm = ({ addItem, disabled = false }: PropsType) => {
-  const { title, changeItemHandler, addItemOnKeyUpHandler, addItemHandler } = useAddItemForm(addItem);
+export const AddItemForm = ({ addItem, buttonText = '+',  disabled = false }: PropsType) => {
+  const { title, error, changeItemHandler, addItemOnKeyUpHandler, addItemHandler } = useAddItemForm(addItem);
   return (
-    <div>
-      <input
+    <div className={styles.addItemFormContainer}>
+      <TextInput
         placeholder="Enter a title"
         value={title}
         onChange={changeItemHandler}
         onKeyUp={addItemOnKeyUpHandler}
         disabled={disabled}
       />
-      <Button onClick={addItemHandler} disabled={disabled}>
-        +
-      </Button>
+      <IconButton iconUrl={addIcon} onClick={addItemHandler} disabled={disabled}/>
     </div>
   )
 }
