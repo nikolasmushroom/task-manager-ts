481d0077fb952c1f36f4e1e72cd97c16d451a335
nikolasmushroom <dashagribok0905@gmail.com>
2024-11-18 23:56:17 +0300

    Was created auth API.

---
+++ src/fuetures/auth/api/auth-api.ts
@@ -0,0 +1,20 @@
+import { baseURL } from "common/instance";
+import { BaseResponse } from "common/types";
+
+export type LoginParams = {
+  email: string,
+  password: string,
+  rememberMe?: boolean,
+  captcha?: string
+}
+export const authAPI = {
+  login(params: LoginParams) {
+    return baseURL.post<BaseResponse<{ userId: number; token: string }>>("/auth/login", params);
+  },
+  logout() {
+    return baseURL.delete<BaseResponse>("/auth/login");
+  },
+  authMe() {
+    return baseURL.get<BaseResponse<{ id: number; email: string; login: string }>>("/auth/me");
+  }
+};
