a35b86878feead3d962a66587fc26505dce37b82
nikolasmushroom <dashagribok0905@gmail.com>
2024-11-19 23:53:13 +0300

    Was added Navigate to Login.tsx in Main.tsx component

--- src/app/Main.tsx
+++ src/app/Main.tsx
@@ -1,22 +1,28 @@
-import { AddItemForm } from "common/components"
+import { AddItemForm } from "common/components";
 import React from "react";
-import { addTodolistTC } from "../model/todolists-reducer"
-import { Todolists } from "../fuetures/todolists/Todolists/Todolists"
-import { useAppDispatch } from "common/hooks"
-import styles from './Main.module.css'
-import { CustomSnackbar } from "common/components/CustomSnackbar";
+import { addTodolistTC } from "../fuetures/todolists/model/todolists-reducer";
+import { Todolists } from "../fuetures/todolists/Todolists/Todolists";
+import { useAppDispatch, useAppSelector } from "common/hooks";
+import styles from "./Main.module.css";
+import { CustomSnackbar } from "common/components/CustomSnackbar/CustomSnackbar";
 import { useSelector } from "react-redux";
 import { RootState } from "./store";
+import { Navigate } from "react-router-dom";
+import { selectIsLoggedIn } from "../fuetures/auth/model/authSelectors";
+import { Path } from "common/router";
 
 export const Main = () => {
-  const dispatch = useAppDispatch()
-  const error = useSelector<RootState, string|null>(state => state.app.error)
-  const isOpen = error !== null
+  const dispatch = useAppDispatch();
+  const isLoggedIn = useAppSelector(selectIsLoggedIn);
+  const error = useSelector<RootState, string | null>(state => state.app.error);
+  const isOpen = error !== null;
   const addTodolist = (title: string) => {
-    const action = addTodolistTC(title)
-    dispatch(action)
+    const action = addTodolistTC(title);
+    dispatch(action);
+  };
+  if (!isLoggedIn) {
+    return <Navigate to={Path.Login} />;
   }
-
   return (
     <div className={styles.background}>
       <div className={styles.container}>
@@ -27,7 +33,9 @@ export const Main = () => {
           <Todolists />
         </div>
       </div>
-      <CustomSnackbar callBack={() => {}} open={isOpen} autoHideDuration={6000} error={error}/>
+      <CustomSnackbar callBack={() => {
+      }} open={isOpen} autoHideDuration={6000} error={error} />
     </div>
-  )
-}
+
+  );
+};
