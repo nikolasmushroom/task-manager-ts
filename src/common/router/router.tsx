2151e4788a2b20a4d989a2607de3ac6413ca63f6
nikolasmushroom <dashagribok0905@gmail.com>
2024-11-20 23:46:55 +0300

    Were added ProtectedRoute wrap and redirect to Page404.tsx

--- src/common/router/router.tsx
+++ src/common/router/router.tsx
@@ -3,29 +3,41 @@ import { createBrowserRouter } from "react-router-dom";
 import { Main } from "../../app/Main";
 import { Login } from "../../fuetures/auth/ui/Login";
 import { Page404 } from "common/components";
+import { Faq } from "../../fuetures/faq/Faq";
+import { ProtectedRoute } from "common/router/ProtectedRoute";
 
 export const Path = {
-  Login: 'login',
-} as const
+  Login: "login"
+} as const;
 
 export const router = createBrowserRouter([
   {
-    path: '/',
-    errorElement : <Page404/>,
+    path: "/",
+    errorElement: <Page404 />,
     element: <App />,
     children: [
       {
-        path: '/',
-        element: <Main />,
+        path: "/",
+        element:
+          <ProtectedRoute>
+            <Main />
+          </ProtectedRoute>
       },
       {
         path: Path.Login,
-        element: <Login />,
+        element: <Login />
       },
       {
-        path: '*',
-        element: <Page404/>,
+        path: "/faq",
+        element:
+          <ProtectedRoute>
+            <Faq />
+          </ProtectedRoute>
       },
-    ],
-  },
-])
+      {
+        path: "*",
+        element: <Page404 />
+      }
+    ]
+  }
+]);
