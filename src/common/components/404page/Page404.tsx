3e6dfe73ecf1833776a09be2d213b0941e7212d3
nikolasmushroom <dashagribok0905@gmail.com>
2024-11-20 23:51:21 +0300

    Was added buttonLink with NavLink to main page.

--- src/common/components/404page/Page404.tsx
+++ src/common/components/404page/Page404.tsx
@@ -1,12 +1,19 @@
-import classes from './Page404.module.css';
-import image404 from './../../../asserts/page-404.svg'
+import classes from "./Page404.module.css";
+import image404 from "./../../../asserts/page-404.svg";
+import { NavLink } from "react-router-dom";
+
 export const Page404 = () => {
-    return (
-        <div className={classes.page}>
-            <img src={image404} alt="404 image"/>
-            <div className={classes.message}>
-                <p>This page wasn't founded</p>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className={classes.page}>
+      <img src={image404} alt="404 image" />
+      <div className={classes.message}>
+        <p>This page wasn't founded</p>
+      </div>
+      <div>
+        <NavLink to={'/'} className={classes.buttonLink}>
+          Return to main page
+        </NavLink>
+      </div>
+    </div>
+  );
+};
