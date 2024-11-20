e3f9b334f55d557ae78266ba20ea846a508ffada
nikolasmushroom <dashagribok0905@gmail.com>
2024-11-20 23:50:40 +0300

    Was added check for isLoggedIn in Header.tsx

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -1,22 +1,27 @@
-import styles from './Header.module.css'
-import menuIconImage from './../../../asserts/burger.svg'
+import styles from "./Header.module.css";
+import menuIconImage from "./../../../asserts/burger.svg";
 import React from "react";
 import { Button } from "common/components/Button/Button";
+import { IconButton } from "common/components/Button/IconButton";
+import { useAppDispatch, useAppSelector } from "common/hooks";
+import { logoutTC } from "../../../fuetures/auth/model/auth-reducer";
+import { selectIsLoggedIn } from "../../../fuetures/auth/model/authSelectors";
 
 export const Header = () => {
-
+  const dispatch = useAppDispatch();
+  const isLoggedIn = useAppSelector(selectIsLoggedIn);
   return (
     <div className={styles.header}>
       <div className={styles.toolBar}>
-        <div className={styles.burgerMenu}>
-          <img src={menuIconImage} alt="burger icon" className={styles.burgerMenuImage}/>
-        </div>
+        <IconButton iconUrl={menuIconImage} className={styles.burgerMenuImage} />
         <div className={styles.buttonContainer}>
-          <Button className={styles.button}>Login</Button>
-          <Button className={styles.button}>Logout</Button>
+          {isLoggedIn && <Button className={styles.button} onClick={() => {
+            dispatch(logoutTC());
+          }}>Logout
+          </Button>}
           <Button className={styles.button}>Faq</Button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
