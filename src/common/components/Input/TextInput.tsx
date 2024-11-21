571b387c740c922a0fb33ba2c4e0a1bb6f461bec
nikolasmushroom <dashagribok0905@gmail.com>
2024-11-21 23:28:33 +0300

    Was added ref to TextInput.tsx

--- src/common/components/Input/TextInput.tsx
+++ src/common/components/Input/TextInput.tsx
@@ -1,11 +1,11 @@
 import React, {
   ChangeEvent,
-  DetailedHTMLProps,
+  DetailedHTMLProps, ForwardedRef, forwardRef,
   InputHTMLAttributes,
   KeyboardEvent,
-  ReactNode,
-} from 'react'
-import s from './TextInput.module.css'
+  ReactNode
+} from "react";
+import s from "./TextInput.module.css";
 
 // тип пропсов обычного инпута
 type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>,
@@ -13,65 +13,64 @@ type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElem
 
 // здесь мы говорим что у нашего инпута будут такие же пропсы как у обычного инпута, кроме type
 // (чтоб не писать value: string, onChange: ...; они уже все описаны в DefaultInputPropsType)
-type SuperInputTextPropsType = Omit<DefaultInputPropsType, 'type'> & {
-  // и + ещё пропсы которых нет в стандартном инпуте
+type SuperInputTextPropsType = Omit<DefaultInputPropsType, "type"> & {
   onChangeText?: (value: string) => void
   onEnter?: () => void
   error?: ReactNode
   spanClassName?: string
+  type?: string
 }
 
-export const TextInput: React.FC<SuperInputTextPropsType> = (
-  {
-    onChange,
-    onChangeText,
-    onKeyPress,
-    onEnter,
-    error,
-    className,
-    spanClassName,
-    id,
+export const TextInput = forwardRef(({
+                                       onChange,
+                                       onChangeText,
+                                       onKeyPress,
+                                       onEnter,
+                                       error,
+                                       className,
+                                       spanClassName,
+                                       id,
+                                       ...restProps
+                                     }: SuperInputTextPropsType, ref: ForwardedRef<HTMLInputElement>) => {
 
-    ...restProps // все остальные пропсы попадут в объект restProps
-  }
-) => {
   const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-    onChange?.(e) // если есть пропс onChange, то передать ему е (поскольку onChange не обязателен)
+    onChange?.(e); // если есть пропс onChange, то передать ему е (поскольку onChange не обязателен)
 
-    onChangeText?.(e.currentTarget.value)
-  }
+    onChangeText?.(e.currentTarget.value);
+  };
   const onKeyPressCallback = (e: KeyboardEvent<HTMLInputElement>) => {
-    onKeyPress?.(e)
+    onKeyPress?.(e);
 
     onEnter && // если есть пропс onEnter
-    e.key === 'Enter' && // и если нажата кнопка Enter
-    onEnter() // то вызвать его
-  }
+    e.key === "Enter" && // и если нажата кнопка Enter
+    onEnter(); // то вызвать его
+  };
 
   const finalSpanClassName = s.error
-    + (spanClassName ? ' ' + spanClassName : '')
+    + (spanClassName ? " " + spanClassName : "");
   const finalInputClassName = s.input
-    + (error ? ' ' + s.errorInput : ' ' + s.superInput)
-    + (className ? ' ' + className : '') // задача на смешивание классов
+    + (error ? " " + s.errorInput : " " + s.superInput)
+    + (className ? " " + className : ""); // задача на смешивание классов
 
   return (
     <div className={s.inputWrapper}>
       <div className={s.errorAndInputContainer}>
-                <span
-                  id={id ? id + '-span' : undefined}
-                  className={finalSpanClassName}
-                >
-                {error}
-            </span>
         <input
           id={id}
-          type={'text'}
+          type={"text"}
           onChange={onChangeCallback}
           onKeyPress={onKeyPressCallback}
           className={finalInputClassName}
-          {...restProps} // отдаём инпуту остальные пропсы если они есть (value например там внутри)
+          ref={ref}
+          {...restProps}
         />
       </div>
+      {/*<div*/}
+      {/*  id={id ? id + "-span" : undefined}*/}
+      {/*  className={finalSpanClassName}*/}
+      {/*>*/}
+      {/*  {error}*/}
+      {/*</div>*/}
     </div>
-  )
-}
+  );
+});
