7ba30a8f99ab2eb80cda9f7029e51037f31e9639
nikolasmushroom <dashagribok0905@gmail.com>
2024-11-24 23:46:16 +0300

    Was added toast using in Main.tsx.

--- src/app/Main.tsx
+++ src/app/Main.tsx
@@ -1,19 +1,27 @@
 import { AddItemForm } from "common/components";
-import React from "react";
+import React, { useEffect } from "react";
 import { addTodolistTC } from "../fuetures/todolists/model/todolists-reducer";
 import { Todolists } from "../fuetures/todolists/Todolists/Todolists";
 import { useAppDispatch, useAppSelector } from "common/hooks";
 import styles from "./Main.module.css";
-import { ErrorToast } from "common/Toast/ErrorToast";
 import { selectAppError } from "./model/selectAppError";
+import { useToast } from "common/components/Toast/ToastContainer";
+import { setAppErrorAC } from "./model/app-reducer";
+
 
 export const Main = () => {
   const dispatch = useAppDispatch();
   const error = useAppSelector(selectAppError);
+  const toast = useToast();
+  if (error) {
+    toast.success(error,"left-bottom", () => dispatch(setAppErrorAC(null)));
+  }
+
   const addTodolist = (title: string) => {
     const action = addTodolistTC(title);
     dispatch(action);
   };
+
   return (
     <div className={styles.background}>
       <div className={styles.container}>
@@ -24,7 +32,6 @@ export const Main = () => {
           <Todolists />
         </div>
       </div>
-      <ErrorToast dispatch={dispatch} error={error}/>
     </div>
 
   );
