c61a265a7370b22e5d1375e14df86ece7edfeb0a
nikolasmushroom <dashagribok0905@gmail.com>
2024-11-28 23:55:42 +0300

    Was created Protected route component

--- src/common/router/ProtectedRoute.tsx
+++ src/common/router/ProtectedRoute.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { ReactNode } from "react";
 import {Navigate} from "react-router-dom"
 import { useAppSelector } from "common/hooks";
-import { selectIsLoggedIn } from "../../fuetures/auth/model/authSelectors";
-
-export const ProtectedRoute = ({children} : any) => {
+import { selectIsLoggedIn } from "../../fuetures/auth/model/authSlice";
+type ProtectedRouteType = {
+  children : ReactNode
+}
+export const ProtectedRoute = ({children} : ProtectedRouteType) => {
   const isLoggedIn = useAppSelector(selectIsLoggedIn)
   if(!isLoggedIn) {
     return <Navigate to="/login"/>
   }
-  return children
+  return <>{ children }</>
 
 };
