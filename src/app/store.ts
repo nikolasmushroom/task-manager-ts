60a1911793479cd56d75395ed9872ef5f83432fb
nikolasmushroom <dashagribok0905@gmail.com>
2024-11-28 23:56:05 +0300

    Was created redux-toolkit store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,25 +1,31 @@
-import { combineReducers, legacy_createStore as createStore, applyMiddleware} from 'redux'
-import { tasksReducer } from '../model/tasks-reducer'
-import { todolistsReducer } from '../fuetures/todolists/model/todolists-reducer'
-import { AppActionTypes, appReducer } from "./model/app-reducer";
-import { thunk, ThunkAction, ThunkDispatch } from "redux-thunk";
-import { authReducer } from "../fuetures/auth/model/auth-reducer";
+import { ThunkAction, thunk } from "redux-thunk";
+import { configureStore } from "@reduxjs/toolkit";
+import { AppActionTypes, appReducer, appSlice } from "./model/appSlice";
+import { tasksReducer, tasksSlice } from "../fuetures/todolists/model/tasksSlice";
+
+import { authReducer, authSlice } from "../fuetures/auth/model/authSlice";
+import { todolistsReducer, todolistsSlice } from "../fuetures/todolists/model/todolistsSlice";
+import { todolistsApi } from "../fuetures/todolists/api/todolists-api";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
 
-const rootReducer = combineReducers({
-    tasks: tasksReducer,
-    todolists: todolistsReducer,
-    app: appReducer,
-    auth: authReducer
-})
-// непосредственно создаём store
-export const store = createStore(rootReducer, {}, applyMiddleware(thunk))
+export const store = configureStore({
+  reducer: {
+    [tasksSlice.name]: tasksReducer,
+    [todolistsSlice.name]: todolistsReducer,
+    [appSlice.name]: appReducer,
+    [authSlice.name]: authReducer,
+    [todolistsApi.reducerPath]: todolistsApi.reducer,
+  },
+  middleware : (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(todolistsApi.middleware),
+});
+setupListeners(store.dispatch)
 
 
 export type RootState = ReturnType<typeof store.getState>
-// export type AppDispatch = typeof store.dispatch
-export type AppDispatch = ThunkDispatch<RootState, unknown, AppActionTypes
->
+
+export type AppDispatch = typeof store.dispatch
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
   RootState,
@@ -28,4 +34,4 @@ export type AppThunk<ReturnType = void> = ThunkAction<
 >
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
-window.store = store
+window.store = store;
