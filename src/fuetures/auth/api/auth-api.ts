55755ab4caf6567d95fa1820dfc7831199c04f8f
nikolasmushroom <dashagribok0905@gmail.com>
2024-12-02 23:33:21 +0300

    Was refactored component auth-api.ts with redux-query

--- src/fuetures/auth/api/auth-api.ts
+++ src/fuetures/auth/api/auth-api.ts
@@ -1,5 +1,5 @@
-import { baseURL } from "common/instance";
 import { BaseResponse } from "common/types";
+import { baseApi } from "../../../app/model/base-api";
 
 export type LoginParams = {
   email: string,
@@ -7,17 +7,28 @@ export type LoginParams = {
   rememberMe?: boolean,
   captcha?: string
 }
-export const authAPI = {
-  login(params: LoginParams) {
-    return baseURL.post<BaseResponse<{ userId: number; token: string }>>("/auth/login", params);
-  },
-  logout() {
-    return baseURL.delete<BaseResponse>("/auth/login");
-  },
-  authMe() {
-    return baseURL.get<BaseResponse<{ id: number; email: string; login: string }>>("/auth/me");
-  },
-  getCaptcha(){
-    return baseURL.get<{url : string}>('/security/get-captcha-url');
-  }
-};
+
+export const authAPI = baseApi.injectEndpoints({
+  endpoints: build => ({
+    me: build.query<BaseResponse<{ id: number; email: string; login: string }>, void>({
+      query: () => "/auth/me"
+    }),
+    getCaptcha: build.mutation<{ url: string }, void>({
+      query: () => "/security/get-captcha-url"
+    }),
+    login: build.mutation<BaseResponse<{ userId: number; token: string }>, LoginParams>({
+      query: (payload) => ({
+        url: "/auth/login",
+        method: "POST",
+        body: payload
+      })
+    }),
+    logout: build.mutation<BaseResponse, void>({
+      query: () => ({
+        url: "/auth/login",
+        method: "DELETE",
+      })
+    })
+  })
+});
+export const {useMeQuery, useLoginMutation, useLogoutMutation, useGetCaptchaMutation} = authAPI
