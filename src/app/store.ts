b14ae1c019e902ace80ccd4fc86685460c221bfe
nikolasmushroom <dashagribok0905@gmail.com>
2024-12-03 23:33:44 +0300

    From store.ts were deleted AppThunk type and slices, which not need now : tasksSlice and todolistsSlice

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,24 +1,16 @@
-import { ThunkAction, thunk } from "redux-thunk";
 import { configureStore } from "@reduxjs/toolkit";
-import { AppActionTypes, appReducer, appSlice } from "./model/appSlice";
-import { tasksReducer, tasksSlice } from "../fuetures/todolists/model/tasksSlice";
-
-import { authReducer, authSlice } from "../fuetures/auth/model/authSlice";
-import { todolistsReducer, todolistsSlice } from "../fuetures/todolists/model/todolistsSlice";
-import { todolistsApi } from "../fuetures/todolists/api/todolists-api";
+import {  appReducer, appSlice } from "./model/appSlice";
 import { setupListeners } from "@reduxjs/toolkit/query";
+import { baseApi } from "./model/base-api";
 
 
 export const store = configureStore({
   reducer: {
-    [tasksSlice.name]: tasksReducer,
-    [todolistsSlice.name]: todolistsReducer,
     [appSlice.name]: appReducer,
-    [authSlice.name]: authReducer,
-    [todolistsApi.reducerPath]: todolistsApi.reducer,
+    [baseApi.reducerPath]: baseApi.reducer,
   },
   middleware : (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(todolistsApi.middleware),
+    getDefaultMiddleware().concat(baseApi.middleware),
 });
 setupListeners(store.dispatch)
 
@@ -26,12 +18,7 @@ setupListeners(store.dispatch)
 export type RootState = ReturnType<typeof store.getState>
 
 export type AppDispatch = typeof store.dispatch
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  AppActionTypes
->
+
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
 window.store = store;
