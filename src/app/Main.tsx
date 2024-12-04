2d29414d006eda807274768b21961f29e9701e55
nikolasmushroom <dashagribok0905@gmail.com>
2024-12-04 16:30:51 +0300

    Main.tsx was refactored.

--- src/app/Main.tsx
+++ src/app/Main.tsx
@@ -1,32 +1,23 @@
 import { AddItemForm } from "common/components";
-import React, { useEffect } from "react";
-import { addTodolistTC } from "../fuetures/todolists/model/todolists-reducer";
-import { Todolists } from "../fuetures/todolists/Todolists/Todolists";
-import { useAppDispatch, useAppSelector } from "common/hooks";
+import { Todolists } from "../fuetures/todolists/ui/Todolists/Todolists";
 import styles from "./Main.module.css";
-import { selectAppError } from "./model/selectAppError";
-import { useToast } from "common/components/Toast/ToastContainer";
-import { setAppErrorAC } from "./model/app-reducer";
+import { useAddTodolistMutation } from "../fuetures/todolists/api/todolists-api";
 
 
 export const Main = () => {
-  const dispatch = useAppDispatch();
-  const error = useAppSelector(selectAppError);
-  const toast = useToast();
-  if (error) {
-    toast.success(error,"left-bottom", () => dispatch(setAppErrorAC(null)));
-  }
 
-  const addTodolist = (title: string) => {
-    const action = addTodolistTC(title);
-    dispatch(action);
+  const [addTodolist] = useAddTodolistMutation();
+
+
+  const addTodolistCallback = (title: string) => {
+    addTodolist(title);
   };
 
   return (
     <div className={styles.background}>
       <div className={styles.container}>
         <div>
-          <AddItemForm addItem={addTodolist} />
+          <AddItemForm addItem={addTodolistCallback} />
         </div>
         <div className={styles.todolistContainer}>
           <Todolists />
