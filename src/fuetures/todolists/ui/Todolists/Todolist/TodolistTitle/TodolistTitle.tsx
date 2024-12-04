cde49ca5ccb44265fda2b102c9ca436453a2bc3c
nikolasmushroom <dashagribok0905@gmail.com>
2024-12-04 16:34:16 +0300

    Logic from TodolistTitle was relocated to custom hook useTodolistTitle.tsx

--- src/fuetures/todolists/ui/Todolists/Todolist/TodolistTitle/TodolistTitle.tsx
+++ src/fuetures/todolists/ui/Todolists/Todolist/TodolistTitle/TodolistTitle.tsx
@@ -1,46 +1,18 @@
 import { EditableSpan } from "common/components";
-import {
-  TodolistDomainType,
-} from "../../../../model/todolistsSlice";
+
 import { useAppDispatch } from "common/hooks";
 import deleteIcon from "../../../../../../asserts/delete.png";
 import { IconButton } from "common/components/Button/IconButton";
 import { todolistsApi, useRemoveTodolistMutation, useUpdateTodolistTitleMutation } from "../../../../api/todolists-api";
 import { StatusType } from "../../../../../../app/model/appSlice";
+import { TodolistDomainType } from "../../../../lib/types/types";
+import { useTodolistTitle } from "../../../../lib/hooks/useTodolistTitle";
 
 type TodolistTitle = {
   todolist: TodolistDomainType
 }
 export const TodolistTitle = ({ todolist }: TodolistTitle) => {
-  const dispatch = useAppDispatch();
-  const [removeTodolist] = useRemoveTodolistMutation();
-  const [updateTodolistTitle] = useUpdateTodolistTitleMutation();
-  const updateQueryData = (status: StatusType) => {
-    dispatch(todolistsApi.util.updateQueryData("getTodolists", undefined, state => {
-      const index = state.findIndex(tl => tl.id === todolist.id);
-      if (index !== -1) {
-        state[index].entityStatus = status;
-      }
-    }));
-  };
-  const updateTodolist = (title: string) => {
-    updateTodolistTitle({ title, id: todolist.id });
-  };
-  const removeTodolistCallback = () => {
-    updateQueryData("loading");
-    removeTodolist(todolist.id)
-      .unwrap()
-      .then((payload) => {
-        if (payload.resultCode === 0) {
-          updateQueryData("succeeded");
-        } else {
-          updateQueryData("failed");
-        }
-      })
-      .catch((error) => {
-        updateQueryData("failed");
-      });
-  };
+  const { updateTodolist, removeTodolistCallback } = useTodolistTitle(todolist);
   return (
     <div className={"todolist-title-container"}>
       <h3>
