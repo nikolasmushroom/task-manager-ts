8468e6999bb6d98279f0095d7b61d888575b9411
nikolasmushroom <dashagribok0905@gmail.com>
2024-12-04 16:11:54 +0300

    Added logout from hook useLogoutMutation. Added invalidation of todolist and tasks cash.

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -4,19 +4,30 @@ import React from "react";
 import { Button } from "common/components/Button/Button";
 import { IconButton } from "common/components/Button/IconButton";
 import { useAppDispatch, useAppSelector } from "common/hooks";
-import { logoutTC } from "../../../fuetures/auth/model/auth-reducer";
-import { selectIsLoggedIn } from "../../../fuetures/auth/model/authSelectors";
+import { selectIsLoggedIn, setIsLoggedIn } from "../../../app/model/appSlice";
+import { useLogoutMutation } from "../../../fuetures/auth/api/auth-api";
+import { baseApi } from "../../../app/model/base-api";
+import { ResultCode } from "../../enums/resultCodeEnum";
 
 export const Header = () => {
   const dispatch = useAppDispatch();
   const isLoggedIn = useAppSelector(selectIsLoggedIn);
+  const [logout] = useLogoutMutation()
   return (
     <div className={styles.header}>
       <div className={styles.toolBar}>
         <IconButton iconUrl={menuIconImage} className={styles.burgerMenuImage} />
         <div className={styles.buttonContainer}>
           {isLoggedIn && <Button className={styles.button} onClick={() => {
-            dispatch(logoutTC());
+            logout().then((result) => {
+              if (result.data?.resultCode === ResultCode.Success) {
+                localStorage.removeItem("sn-token");
+              }
+              dispatch(setIsLoggedIn({ isLoggedIn: false }));
+            }).then(() => {
+              baseApi.util.invalidateTags(['Todolist'])
+              baseApi.util.invalidateTags(['Tasks'])
+            })
           }}>Logout
           </Button>}
           <Button className={styles.button}>Faq</Button>
