a1a4da14443d605b94ae0e488ca154ee6d1cb147
nikolasmushroom <dashagribok0905@gmail.com>
2024-12-07 23:47:25 +0300

    Was refactored component App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,40 +1,14 @@
 import "./App.css";
 import { Header } from "common/components";
 import { Outlet } from "react-router-dom";
-import React, { useEffect, useState } from "react";
-import { useAppDispatch, useAppSelector } from "common/hooks";
 import { Preloader } from "common/components/Preloader/Preloader";
-import { selectAppError, selectAppStatus, setAppError, setIsLoggedIn } from "./model/appSlice";
-import { useMeQuery } from "../fuetures/auth/api/auth-api";
-import { useToast } from "common/components/Toast/ToastContainer";
-import { ResultCode } from "common/enums/resultCodeEnum";
+import { useInitializeApp } from "common/hooks/useApp";
 
 
 export const App = () => {
-  const status = useAppSelector(selectAppStatus);
-  const [isInitialized, setIsInitialized] = useState(false);
-  const dispatch = useAppDispatch();
-  const appError = useAppSelector(selectAppError);
-  const toast = useToast();
-  if (appError) {
-    toast.error(appError, "left-bottom", () => {
-      dispatch(setAppError({ error: null }));
-    });
-  }
-  const { data, isLoading } = useMeQuery();
-  useEffect(() => {
-    if (!isLoading) {
-      setIsInitialized(true);
-      if (data?.resultCode === ResultCode.Success) {
-        dispatch(setIsLoggedIn({ isLoggedIn: true }));
-      }
-    }
-  }, [isLoading, data, status]);
-
+  const {isInitialized} = useInitializeApp();
   return (
-
     <div>
-
       {!isInitialized ? <Preloader /> : <>
         <Header />
         <Outlet />
